f968ae2cb2b6e699ccc6e421413b77e5f3604317
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-03-24 18:48:58 -0300

    feat: :construction: implement layout structure with Body and Header components

--- event/lo-de-charly-tcg-2026/astro.config.mjs
+++ event/lo-de-charly-tcg-2026/astro.config.mjs
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -8,5 +8,27 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Press Start 2P',
+      cssVariable: '--fontsource-press-start-2p',
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: 'VT323',
+      cssVariable: '--fontsource-vt323',
+    }
+  ]
 });
+
+// import { defineConfig, fontProviders } from "astro/config";
+
+// export default defineConfig({
+//   fonts: [{
+//     provider: fontProviders.fontsource(),
+//     name: "Roboto",
+//     cssVariable: "--font-roboto",
+//   }]
+// });
