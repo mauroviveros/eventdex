5dba5bcd380521d22e41bfc39ecede6145c2cf10
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-03-29 00:29:30 -0300

    feat: :sparkles: init Hero

--- event/lodecharlytcg/astro.config.mjs
+++ event/lodecharlytcg/astro.config.mjs
@@ -1,26 +1,26 @@
-import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, fontProviders } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
   },
   fonts: [
     {
       provider: fontProviders.fontsource(),
-      name: 'Press Start 2P',
-      cssVariable: '--fontsource-press-start-2p',
+      name: "Press Start 2P",
+      cssVariable: "--fontsource-press-start-2p",
     },
     {
       provider: fontProviders.fontsource(),
-      name: 'VT323',
-      cssVariable: '--fontsource-vt323',
+      name: "VT323",
+      cssVariable: "--fontsource-vt323",
     }
-  ]
+  ],
 });
 
 // import { defineConfig, fontProviders } from "astro/config";
