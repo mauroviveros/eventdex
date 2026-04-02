4e61e4630df3c9abb94d9ef4781aaf378d5f1ed1
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-04-02 16:35:33 -0300

    feat: :sparkles: implement dropdown menu for user profile actions

---
+++ event/lodecharlytcg.com/src/components/account/profile.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import { createClient } from "@/libs/supabase/client"
+import { ChevronDown, Logout, User as UserIcon } from "@nsmr/pixelart-react"
+import { User } from "@supabase/supabase-js"
+import Image from "next/image"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { useMemo } from "react"
+import { Button } from "../ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
+
+export const Profile = ({ user }: { user: User }) => {
+  const supabase = useMemo(() => createClient(), [])
+  const router = useRouter()
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut()
+    router.refresh()
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="h-14 rounded-none max-w-full" type="button" variant="default">
+          <Image
+            src={user.user_metadata.avatar_url}
+            alt={user.user_metadata.full_name}
+            width={40}
+            height={40}
+            loading="lazy"
+            decoding="async"
+            className="size-10"
+          />
+
+          <div className="hidden md:flex flex-col min-w-0 text-left">
+            <span className="text-xl leading-5 font-bold truncate">{user.user_metadata.full_name}</span>
+            <span className="text-lg leading-3 opacity-70 truncate">{user.email}</span>
+          </div>
+
+          <ChevronDown />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="rounded-none">
+        <DropdownMenuItem variant="default" asChild>
+          <Link href="/perfil">
+            <UserIcon />
+            Mi perfil
+          </Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem variant="destructive" onSelect={handleSignOut}>
+          <Logout />
+          Cerrar sesión
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
---
+++ event/lodecharlytcg.com/src/components/account/signin.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { signInWithGoogle } from "@/app/api/auth/signin/actions"
+import { GoogleOriginalIcon } from "@devicon/react"
+import { useTransition } from "react"
+import { Button } from "../ui/button"
+
+export const SignIn = () => {
+  const [isSigningIn, startSignInTransition] = useTransition()
+
+  const handleSignInWithGoogle = () => {
+    startSignInTransition(async () => {
+      await signInWithGoogle()
+    })
+  }
+
+  return (
+    <Button
+      type="button"
+      className="h-full rounded-none border-y-0 max-w-full w-full"
+      onClick={handleSignInWithGoogle}
+      variant="outline"
+      disabled={isSigningIn}
+    >
+      <GoogleOriginalIcon className="text-2xl" />
+      <span className="text-xl font-bold hidden md:block">Iniciar sesión</span>
+    </Button>
+  )
+}
