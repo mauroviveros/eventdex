f0cc5a1bd007525d3c0268be87378f535e467b57
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-04-04 20:14:19 -0300

    feat: :mag: add favicons

--- event/lodecharlytcg.com/src/app/layout.tsx
+++ event/lodecharlytcg.com/src/app/layout.tsx
@@ -23,6 +23,14 @@ const pressStart = Press_Start_2P({
 export const metadata: Metadata = {
   title: "Lo de Charly TCG | Eventdex",
   description: "Evento de cartas coleccionables TCG",
+  icons: {
+    apple: '/apple-touch-icon.png',
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
