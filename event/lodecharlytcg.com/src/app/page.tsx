4e470e7d724857483c79959c6fdd5264ce8f96eb
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-04-04 21:49:52 -0300

    feat: ✨ Add Ranking component and integrate it into the Home page layout

--- event/lodecharlytcg.com/src/app/page.tsx
+++ event/lodecharlytcg.com/src/app/page.tsx
@@ -1,8 +1,9 @@
 import { Countdown } from "@/components/countdown";
+import Ranking from "@/components/ranking";
 import { Card, CardContent } from "@/components/ui/card";
 import { SITE_SLUG } from "@/constants";
 import { createClient } from "@/libs/supabase/server";
-import { formatScheduleLabel } from "@/utils";
+import { cn, formatScheduleLabel } from "@/utils";
 import { Clock } from "@nsmr/pixelart-react";
 import { DateTime } from "luxon";
 import { cookies } from "next/headers";
@@ -22,33 +23,45 @@ export default async function Home() {
   const hasEventStarted = startDateTime <= DateTime.now();
 
   return (
-    <section className="flex-1 min-h-[calc(100dvh-4rem)] flex flex-col items-center justify-center px-2 py-8 gap-8">
-      <div className="max-w-3xl mx-auto flex flex-col items-center">
-        <Card size="sm" className="mx-4">
-          <CardContent className="flex items-center gap-2">
-            <Clock className="text-muted-foreground size-5" />
-            <span className="text-xl text-accent">{formatScheduleLabel(event?.schedules[0])}</span>
-          </CardContent>
-        </Card>
+    <>
+      <section
+        className={cn([
+          "flex-1 flex flex-col items-center justify-center px-2 py-8 gap-8",
+          hasEventStarted ? "min-h-[calc(85dvh-5rem)]" : "min-h-[calc(100dvh-5rem)]",
+        ])}
+      >
+        <div className="max-w-3xl mx-auto flex flex-col items-center">
+          <Card size="sm" className="mx-4">
+            <CardContent className="flex items-center gap-2">
+              <Clock className="text-muted-foreground size-5" />
+              <span className="text-xl text-accent">{formatScheduleLabel(event?.schedules[0])}</span>
+            </CardContent>
+          </Card>
 
-        <Card className="highlight z-10">
-          <CardContent className="font-press-start text-center space-y-1 px-2">
-            <h1 className="text-3xl text-secondary text-balance">{config?.title}</h1>
-            <p className="text-xs text-muted-foreground text-pretty">{config?.description}</p>
-          </CardContent>
-        </Card>
+          <Card className="highlight z-10">
+            <CardContent className="font-press-start text-center space-y-1 px-2">
+              <h1 className="text-3xl text-secondary text-balance">{config?.title}</h1>
+              <p className="text-xs text-muted-foreground text-pretty">{config?.description}</p>
+            </CardContent>
+          </Card>
 
-        <Card size="sm" className="mx-4">
-          <CardContent>
-            <h3 className="text-xl text-accent">{event?.location?.name}</h3>
-            <p className="text-muted-foreground">{event?.location?.address}</p>
-          </CardContent>
-        </Card>
-      </div>
+          <Card size="sm" className="mx-4">
+            <CardContent>
+              <h3 className="text-xl text-accent">{event?.location?.name}</h3>
+              <p className="text-muted-foreground">{event?.location?.address}</p>
+            </CardContent>
+          </Card>
+        </div>
 
-      {!hasEventStarted && (
-        <Countdown start_datetime={event.schedules[0].start_datetime} initial={DateTime.now().toMillis()} />
+        {!hasEventStarted && (
+          <Countdown start_datetime={event.schedules[0].start_datetime} initial={DateTime.now().toMillis()} />
+        )}
+      </section>
+      {hasEventStarted && (
+        <div className="w-full max-w-3xl mx-auto px-4">
+          <Ranking />
+        </div>
       )}
-    </section>
+    </>
   )
 }
